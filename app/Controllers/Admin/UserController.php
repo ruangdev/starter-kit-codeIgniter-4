115f35d40fe01c1ac0f1c573e98b2f8a0a43fb20
IDrumahdev <ibnudirsan@gmail.com>
2023-12-21 16:50:28 +0700

    UserValidation

--- app/Controllers/Admin/UserController.php
+++ app/Controllers/Admin/UserController.php
@@ -7,6 +7,7 @@ use App\Helpers\UUID;
 use App\Models\Users;
 use Myth\Auth\Password;
 use App\Models\UserProfile;
+use App\Validation\UserValidation;
 use \Hermawan\DataTables\DataTable;
 use App\Controllers\BaseController;
 use App\Repository\User\UserResponse;
@@ -44,49 +45,31 @@ class UserController extends BaseController
 
     public function store()
     {
-        $validationRules = [
-            'username' => 'required|min_length[8]',
-            'email'    => 'required|valid_email|is_unique[users.email]',
-            'password' => 'required|min_length[8]',
-        ];
-
-        // Pesan kustom untuk validasi khusus
-        $validationMessages = [
-            'username' => [
-                'min_length' => 'Username sudah digunakan, silakan pilih yang lain.'
-            ],
-            'email' => [
-                'is_unique' => 'Email sudah terdaftar, gunakan email lain.'
-            ],
-            'password' => [
-                'min_length' => 'Panjang password minimal 8 karakter.'
-            ],
-        ];
-
-        $validation = Services::validation();
+        $validationRules    = UserValidation::rules();
+        $validationMessages = UserValidation::messages();
+        $validation         = Services::validation();
         if (!$this->validate($validationRules, $validationMessages)) {
             return redirect()->to(route_to('admin.user.create'))->withInput()->with('errors', $validation->getErrors());
         }
-
-        try {
-            $param = $this->request->getRawInput();
-            $this->UserResponse->create($param);
-            $notification = [
-                'message'     => 'Successfully created Admin.',
-                'alert-type'  => 'success',
-                'gravity'     => 'bottom',
-                'position'    => 'right'
-            ]; 
-                return redirect()->to(route_to('admin.user.list'))->with('message', $notification);
-        } catch (\Throwable $th) {
-            $notification = [
-                'message'     => 'Failed to create Admin.',
-                'alert-type'  => 'danger',
-                'gravity'     => 'bottom',
-                'position'    => 'right'
-            ];   
-                return redirect()->to(route_to('admin.user.list'))->with('message', $notification);
-        }
+            try {
+                $param = $this->request->getRawInput();
+                $this->UserResponse->create($param);
+                $notification = [
+                    'message'     => 'Successfully created Admin.',
+                    'alert-type'  => 'success',
+                    'gravity'     => 'bottom',
+                    'position'    => 'right'
+                ]; 
+                    return redirect()->to(route_to('admin.user.list'))->with('message', $notification);
+            } catch (\Throwable $th) {
+                $notification = [
+                    'message'     => 'Failed to create Admin.',
+                    'alert-type'  => 'danger',
+                    'gravity'     => 'bottom',
+                    'position'    => 'right'
+                ];   
+                    return redirect()->to(route_to('admin.user.list'))->with('message', $notification);
+            }
     }
 
     public function edit($id)
---
+++ app/Validation/UserValidation.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Validation;
+
+use CodeIgniter\Validation\Rules;
+
+class UserValidation
+{
+    public static function rules(): array
+    {
+        return [
+            'fullName'    => 'required|max_length[50]',
+            'name'        => 'required|min_length[5]|max_length[50]',
+            'telegramid'  => 'required|min_length[5]|max_length[20]',
+            'Numberphone' => 'required|regex_match[/^08[0-9]{8,15}$/]',
+            'password'    => 'required|min_length[6]',
+        ];
+    }
+
+    public static function messages(): array
+    {
+        return [
+            'fullName' => [
+                'required'    => 'Nama lengkap harus diisi.',
+                'max_length'  => 'Panjang karakter nama lengkap maksimal 50.'
+            ],
+            'name' => [
+                'required'    => 'Username harus diisi.',
+                'min_length'  => 'Panjang karakter Username minimal 5.',
+                'max_length'  => 'Panjang karakter Username maksimal 50.'
+            ],
+            'telegramid' => [
+                'required'    => 'Telegram ID harus diisi.',
+                'min_length'  => 'Panjang karakter Telegram ID minimal 5.',
+                'max_length'  => 'Panjang karakter Telegram ID maksimal 20.'
+            ],
+            'Numberphone' => [
+                'required'    => 'Nomor Telepon harus diisi.',
+                'regex_match' => 'Harus diawali dengan 08 dan panjang minimal 10 dan maksimal 15 karakter.'
+            ],
+            'password' => [
+                'required'    => 'Password harus diisi.',
+                'min_length'  => 'Panjang karakter password minimal 6.'
+            ],
+        ];
+    }
+}
